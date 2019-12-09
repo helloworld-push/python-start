df2cb1d676e778f9cc2102101a5ffcf647d9f31e
helloworld-push <kind.rand0m@yandex.ru>
2019-12-09 22:57:42 +0300

    second commit

---
+++ penguin.py
@@ -0,0 +1,11 @@
+head = '   _~_    '
+eye = '  (o o)   '
+beak = ' /  V  \  '
+body = '/(  _  )\ '
+legs = '  ^^ ^^   '
+count = int(input())
+print(head * count)
+print(eye * count)
+print(beak * count)
+print(body * count)
+print(legs * count)
